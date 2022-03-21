aee8051dd888a06db309db508718004f77c653c7
shilohooo <Lixiaolei595@gmail.com>
2022-03-21 11:36:37 +0800

    docs: 添加注释笔记

--- quick-start/src/test/java/org/shiloh/test/SqlSessionFactoryTests.java
+++ quick-start/src/test/java/org/shiloh/test/SqlSessionFactoryTests.java
@@ -23,6 +23,35 @@ public class SqlSessionFactoryTests {
 
     /**
      * 测试从XML文件中构建 {@link org.apache.ibatis.session.SqlSessionFactory}
+     * <p>
+     * {@link SqlSessionFactoryBuilder}的主要作用是构建{@link SqlSessionFactory}，当构建完成后便可以舍弃掉
+     * {@link SqlSessionFactoryBuilder}，因此官方建议将{@link SqlSessionFactoryBuilder}设置为局部变量即可。
+     * <p>
+     * 以下是官方对{@link SqlSessionFactoryBuilder}作用域和生命周期的说明：
+     * <p>
+     * 这个类可以被实例化、使用和丢弃，一旦创建了 SqlSessionFactory，就不再需要它了。
+     * <p>
+     * 因此 SqlSessionFactoryBuilder 实例的最佳作用域是方法作用域（也就是局部方法变量）。
+     * <p>
+     * 你可以重用 SqlSessionFactoryBuilder 来创建多个 SqlSessionFactory 实例，
+     * <p>
+     * 但最好还是不要一直保留着它，以保证所有的 XML 解析资源可以被释放给更重要的事情。
+     * <p>
+     * {@link SqlSessionFactory}是 MyBatis应用的核心，可以从中获取{@link org.apache.ibatis.session.SqlSession}的实例，
+     * {@link org.apache.ibatis.session.SqlSession}提供了在数据库执行SQL命令所需要的所有方法，
+     * 可以通过{@link org.apache.ibatis.session.SqlSession}来执行已映射的SQL语句。
+     * <p>
+     * 对于{@link SqlSessionFactory}，每个 MyBatis 应用应该只创建一次，且仅存在唯一实例，此处建议使用单例模式实现。
+     * <p>
+     * 官方对于{@link SqlSessionFactory}作用域和生命周期的说明如下：
+     * <p>
+     * {@link SqlSessionFactory}一旦被创建就应该在应用的运行期间一直存在，没有任何理由丢弃它或重新创建另一个实例。
+     * <p>
+     * 使用 {@link SqlSessionFactory} 的最佳实践是在应用运行期间不要重复创建多次，
+     * <p>
+     * 多次重建 {@link SqlSessionFactory} 被视为一种代码“坏习惯”。因此 {@link SqlSessionFactory} 的最佳作用域是应用作用域。
+     * <p>
+     * 有很多方法可以做到，最简单的就是使用单例模式或者静态单例模式。
      *
      * @author shiloh
      * @date 2022/3/19 12:24
--- quick-start/src/test/java/org/shiloh/test/SqlSessionTests.java
+++ quick-start/src/test/java/org/shiloh/test/SqlSessionTests.java
@@ -18,6 +18,20 @@ import java.io.InputStream;
 public class SqlSessionTests {
     /**
      * 从{@link org.apache.ibatis.session.SqlSessionFactory}中获取{@link org.apache.ibatis.session.SqlSession}
+     * <p>
+     * 每个线程都应该有它自己的 {@link SqlSession} 实例。{@link SqlSession} 的实例不是线程安全的，因此是不能被共享的，
+     * <p>
+     * 所以它的最佳的作用域是请求或方法作用域。 绝对不能将 {@link SqlSession} 实例的引用放在一个类的静态域，
+     * <p>
+     * 甚至一个类的实例变量也不行。 也绝不能将 {@link SqlSession} 实例的引用放在任何类型的托管作用域中，
+     * <p>
+     * 比如 Servlet 框架中的 HttpSession。 如果你现在正在使用一种 Web 框架，考虑将 SqlSession 放在一个和 HTTP 请求相似的作用域中。
+     * <p>
+     * 换句话说，每次收到 HTTP 请求，就可以打开一个 SqlSession，返回一个响应后，就关闭它。
+     * <p>
+     * 这个关闭操作很重要，为了确保每次都能执行关闭操作，你应该把这个关闭操作放到 finally 块中。
+     * <p>
+     * 可以使用try-with-resources来操作。
      *
      * @author shiloh
      * @date 2022/3/20 12:18
