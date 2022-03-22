68be11b0e13b35791f0ed8875be360a5180cb719
shiloh <lixiaolei595@gmail.com>
2022-03-22 19:13:51 +0800

    feat: 新增查询方法：根据ID查询用户信息

--- quick-start/src/main/java/org/shiloh/mapper/UserMapper.java
+++ quick-start/src/main/java/org/shiloh/mapper/UserMapper.java
@@ -1,5 +1,6 @@
 package org.shiloh.mapper;
 
+import org.apache.ibatis.annotations.Param;
 import org.shiloh.entity.User;
 
 import java.util.List;
@@ -19,4 +20,14 @@ public interface UserMapper {
      * @date 2022/3/20 12:38
      */
     List<User> findAll();
+
+    /**
+     * 根据ID查询用户信息
+     *
+     * @param id ID
+     * @return 用户信息
+     * @author shiloh
+     * @date 2022/3/22 19:13
+     */
+    User findById(@Param("id") Long id);
 }
