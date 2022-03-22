bd6ac15385732f99c3efa4a90d3383b2f6952697
shiloh <lixiaolei595@gmail.com>
2022-03-22 19:27:43 +0800

    test: 新增测试用例：根据ID查询用户信息

--- quick-start/src/test/java/org/shiloh/test/UserMapperTests.java
+++ quick-start/src/test/java/org/shiloh/test/UserMapperTests.java
@@ -22,21 +22,21 @@ import java.util.List;
 public class UserMapperTests {
     /**
      * 测试获取所有用户信息
-     *
+     * <p>
      * 官方对映射器实例作用域和生命周期的说明：
-     *
+     * <p>
      * 映射器是一些绑定映射语句的接口。映射器接口的实例是从 SqlSession 中获得的。
-     *
+     * <p>
      * 虽然从技术层面上来讲，任何映射器实例的最大作用域与请求它们的 SqlSession 相同。
-     *
+     * <p>
      * 但方法作用域才是映射器实例的最合适的作用域。
-     *
+     * <p>
      * 也就是说，映射器实例应该在调用它们的方法中被获取，使用完毕之后即可丢弃。
-     *
+     * <p>
      * 映射器实例并不需要被显式地关闭。尽管在整个请求作用域保留映射器实例不会有什么问题，
-     *
+     * <p>
      * 但是你很快会发现，在这个作用域上管理太多像 SqlSession 的资源会让你忙不过来。
-     *
+     * <p>
      * 因此，最好将映射器放在方法作用域内。
      *
      * @author shiloh
@@ -57,6 +57,30 @@ public class UserMapperTests {
             final List<User> users = userMapper.findAll();
             Assert.assertFalse(users.isEmpty());
             users.forEach(System.out::println);
+        }
+    }
+
+    /**
+     * 测试根据ID获取用户信息
+     *
+     * @author shiloh
+     * @date 2022/3/22 19:15
+     */
+    @Test
+    public void testGetUserById() throws Exception {
+        // 加载mybatis配置文件
+        final InputStream inputStream = Resources.getResourceAsStream("mybatis-config.xml");
+        // 实例化SqlSessionFactory
+        final SqlSessionFactory factory = new SqlSessionFactoryBuilder()
+                .build(inputStream);
+        // 从工厂中获取SqlSession
+        try (final SqlSession sqlSession = factory.openSession()) {
+            // 从Session中获取Mapper
+            final UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+            // 执行SQL
+            final User user = userMapper.findById(1L);
+            Assert.assertNotNull(user);
+            System.out.println(user);
         }
     }
 }
