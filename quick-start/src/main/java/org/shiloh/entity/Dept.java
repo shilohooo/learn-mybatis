87f9991f1d4097256f44a52b897c8d7dd0841338
shiloh <lixiaolei595@gmail.com>
2022-04-09 21:07:17 +0800

    feat: toString 方法忽略 users 列表

--- quick-start/src/main/java/org/shiloh/entity/Dept.java
+++ quick-start/src/main/java/org/shiloh/entity/Dept.java
@@ -1,6 +1,7 @@
 package org.shiloh.entity;
 
 import lombok.Data;
+import lombok.ToString;
 
 import java.util.List;
 
@@ -11,6 +12,7 @@ import java.util.List;
  * @date 2022/4/8 22:38
  */
 @Data
+@ToString(exclude = {"users"})
 public class Dept {
     /**
      * ID
