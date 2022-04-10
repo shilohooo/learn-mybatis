6815cc80f2fa8bb48652ac36c88e160c09c7eb98
shiloh <lixiaolei595@gmail.com>
2022-04-10 14:58:33 +0800

    feat: 新增部门 Mapper

--- quick-start/src/main/java/org/shiloh/mapper/DeptMapper.java
+++ quick-start/src/main/java/org/shiloh/mapper/DeptMapper.java
@@ -1,9 +1,22 @@
 package org.shiloh.mapper;
 
+import org.apache.ibatis.annotations.Param;
+import org.shiloh.entity.Dept;
+
 /**
  * 部门 Mapper
+ *
  * @author shiloh
  * @date 2022/4/8 22:41
  */
 public interface DeptMapper {
+    /**
+     * 根据ID查询部门信息, 以及该部门下的所有用户
+     *
+     * @param id ID
+     * @return 部门信息, 以及该部门下的所有用户
+     * @author shiloh
+     * @date 2022/4/10 13:36
+     */
+    Dept findByIdJoinUser(@Param("id") Long id);
 }
