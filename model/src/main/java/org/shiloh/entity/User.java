bb181050d4c1ab495b8d11af4648a67d73add135
shilohooo <Lixiaolei595@gmail.com>
2022-04-18 10:40:11 +0800

    fix: 实现序列化标记接口，修复缓存报错bug

--- model/src/main/java/org/shiloh/entity/User.java
+++ model/src/main/java/org/shiloh/entity/User.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 /**
  * 用户信息实体
  *
@@ -13,7 +15,9 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+public class User implements Serializable {
+    private static final long serialVersionUID = -4096114430354729824L;
+
     /**
      * ID
      */
